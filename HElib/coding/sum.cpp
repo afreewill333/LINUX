ff5cf1d9eec4463439dff3cec26a6c77d7d0b8c8
John Fisher Smith <afreewill.me@gmail.com>
2018-09-23 11:58:19 +0800

    2 3 ...  +   4 5 .... by 3 ways

---
+++ HElib/coding/sum.cpp
@@ -0,0 +1,222 @@
+#include <cstddef>
+#include <sys/time.h>
+#include "FHE.h"
+#include "EncryptedArray.h"
+#include <NTL/ZZX.h>
+#include <NTL/ZZ.h>
+#include <gmp.h>
+
+#define VEC_SIZE 144
+
+void noPackingSum(long u[], long v[], long result[], FHESecKey sk, FHEPubKey pk)
+{
+    // Vectors to hold the ciphertexts created from the elements of u and v
+    std::vector<Ctxt> encU(VEC_SIZE, Ctxt(pk));
+    std::vector<Ctxt> encV(VEC_SIZE, Ctxt(pk));
+
+    // Each element is encrypted individually
+    #pragma omp parallel for
+    for (int i = 0; i < VEC_SIZE; i++) {
+	Ctxt tempU(pk);
+	pk.Encrypt(tempU, to_ZZX(u[i]));
+	encU[i] = tempU;
+
+	Ctxt tempV(pk);
+	pk.Encrypt(tempV, to_ZZX(v[i]));
+	encV[i] = tempV;
+    }
+
+    std::cout << "HElib No packing encryption: " << "OK!" <<  std::endl;
+
+    // Sum vectors element wise
+    // The sum is stored in U
+    #pragma omp parallel for
+
+    for (int i = 0; i < VEC_SIZE; i++) {
+	encU[i] += encV[i];
+    }
+
+    std::cout << "HElib No packing sum: " << "OK!"<<  std::endl;
+
+    // Decrypt result
+    #pragma omp parallel for
+    for (int i = 0; i < VEC_SIZE; i++) {
+	ZZX element;
+	sk.Decrypt(element, encU[i]);
+	result[i] = conv<long>(element[0]);
+    }
+
+    std::cout << "HElib No packing decryption: " << "OK!" <<  std::endl;
+
+}
+
+void packingPolySum(long u[], long v[], long result[], FHESecKey sk, FHEPubKey pk)
+{
+
+    // ZZX is a class for polynomials from the NTL library
+    ZZX U, V;                        
+
+    // Set the length of the polynomial U(x) and V(x)
+    U.SetLength(VEC_SIZE);
+    V.SetLength(VEC_SIZE);
+
+    // Set the coefficients of the polynomials U(x) and V(x).
+    for (int i = 0; i < VEC_SIZE; i++) {
+	SetCoeff(U, i, u[i]);
+	SetCoeff(V, i, v[i]);	
+    }
+
+    // Ciphertexts that will hold the polynomials encrypted using public key pk
+    Ctxt encU(pk);                          
+    Ctxt encV(pk);                          
+
+    // Encrypt the polynomials into the ciphertexts
+    pk.Encrypt(encU, U);
+    pk.Encrypt(encV, V);
+
+    std::cout << "HElib poly packing encryption: " << "OK!" <<  std::endl;
+
+    // Multiply the ciphertexts and store the result into encU
+    encU += encV;
+
+    std::cout << "HElib poly packing sum: " << "OK!" <<  std::endl;    
+
+    // Decrypt the multiplied ciphertext into a polynomial using the secret key sk
+    ZZX resultPoly;
+    sk.Decrypt(resultPoly, encU);
+
+    // Assign the values of the polynomial's coefficients to the result vector
+    #pragma omp parallel for
+    for (int i = 0; i < VEC_SIZE; i++) {
+	result[i] = conv<long>(resultPoly[i]);
+    }
+
+    std::cout << "HElib poly packing decryption: " << "OK!" <<  std::endl;
+
+}
+
+void packingSubfieldSum(long u[], long v[], long result[], FHEPubKey pk, FHESecKey sk, FHEcontext& context)
+{
+    // Creates a helper object based on the context
+    EncryptedArray ea(context, context.alMod.getFactorsOverZZ()[0]); 
+
+    // The vectors should have the same size as the EncryptedArray (ea.size),
+    // so fill the other positions with 0 which won't change the result
+    std::vector<long> U(u, u + VEC_SIZE);
+    std::vector<long> V(v, v + VEC_SIZE);
+
+    for (int i = VEC_SIZE; i < ea.size(); i++) {
+	U.push_back(0);
+	V.push_back(0);
+    }
+
+    // Ciphertexts that will hold the encrypted vectors
+    Ctxt encU(pk);
+    Ctxt encV(pk);
+
+    // Encrypt the whole vector into one ciphertext using packing
+    ea.encrypt(encU, pk, U);
+    ea.encrypt(encV, pk, V);
+
+    std::cout << "HElib subfield packing encryption: " << "OK!" <<  std::endl;    
+
+    // Sum ciphertexts and set the result to encU
+    encU += encV;
+    std::cout << "HElib subfield packing sum: " << "OK!" <<  std::endl;        
+  
+    // Decrypt the result
+    std::vector<long> res(ea.size(), 0);
+    ea.decrypt(encU, sk, res);   
+
+    // Assign the values of the polynomial's coefficients to the result vector
+    #pragma omp parallel for
+    
+    for (int i = 0; i < VEC_SIZE; i++) {
+	result[i] = conv<long>(res[i]);
+    }
+
+    std::cout << "HElib subfield packing decryption: " << "OK!" <<  std::endl;            
+
+}
+
+
+int main(int argc, char **argv)
+{
+
+    /*** BEGIN INITIALIZATION ***/
+
+    long m = 0;                   // Specific modulus
+
+    long p = 100001651;                 // Plaintext base [default=2], should be a prime number
+
+    long r = 1;                   // Lifting [default=1]
+
+    long L = 10;                  // Number of levels in the modulus chain [default=heuristic]
+
+    long c = 2;                   // Number of columns in key-switching matrix [default=2]
+
+    long w = 64;                  // Hamming weight of secret key
+
+    long d = 1;                   // Degree of the field extension [default=1]
+
+    long k = 80;                  // Security parameter [default=80] 
+
+    long s = 0;                   // Minimum number of slots [default=0]
+  
+
+    std::cout << "Finding m... " << std::flush;
+    m = FindM(k, L, c, p, d, s, 0);           // Find a value for m given the specified values
+    std::cout << "m = " << m << std::endl;
+
+    std::cout << "Initializing context... " << std::flush;
+    FHEcontext context(m, p, r); 	          // Initialize context
+    buildModChain(context, L, c);             // Modify the context, adding primes to the modulus chain
+    std::cout << "OK!" << std::endl;
+
+    std::cout << "Generating keys... " << std::flush;
+    FHESecKey sk(context);                    // Construct a secret key structure
+    const FHEPubKey& pk = sk;                 // An "upcast": FHESecKey is a subclass of FHEPubKey
+    sk.GenSecKey(w);                          // Actually generate a secret key with Hamming weight
+    addSome1DMatrices(sk);                    // Extra information for relinearization
+    std::cout << "OK!" << std::endl;
+
+    // Arrays whose elements will be the coefficients of the polynomials U(x) and V(x)
+    long u[VEC_SIZE];
+    long v[VEC_SIZE];
+    long result[VEC_SIZE];    
+
+    // Initialize arrays
+    // Array u will have even numbers, and array v will have odd numbers
+    for (int i = 0; i < VEC_SIZE; i++) {
+	u[i] = 2*i;
+	v[i] = 2*i + 1;
+    }
+    std::cout << "Time taken for the initialization: " << "OK!" << std::endl;
+
+    /*** METHOD 1: SUM ARRAYS USING HELIB WITHOUT PACKING ***/
+    noPackingSum(u, v, result, sk, pk);
+    std::cout << "HElib without packing method done in " << "OK" <<  std::endl;	
+
+    /*** METHOD 2: SUM ARRAYS USING HELIB WITH POLYNOMIAL PACKING***/
+    packingPolySum(u, v, result, sk, pk);
+    std::cout << "HElib with polynomial packing method done in " << "OK!" <<  std::endl;
+
+    /*** METHOD 3: SUM ARRAYS USING HELIB WITH SUBFIELD PACKING ***/
+    packingSubfieldSum(u, v, result, pk, sk, context);
+    std::cout << "HElib with subfield packing method done in " << "OK!" <<  std::endl;
+
+
+    // Checking results
+    for (int i = 0; i < VEC_SIZE; i++) {
+        std::cout << u[i] << "+" << v[i] << " = " 
+		<< u[i]+v[i] << " == "<<  result[i] << std::endl;
+	if( u[i]+v[i] != result[i] )
+		cout<<endl<<endl
+			<<"==============ERROR==============="<<endl<<endl;
+    }
+
+    cout<<" TEST SUCCESSFULL"<<endl;
+
+    return 0;
+
+}
